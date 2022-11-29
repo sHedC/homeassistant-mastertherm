285a79a78a0e04af5b8b667fe01ad691a7a0d972
Richard Holmes <richard@shedc.uk>
2022-11-29 05:15:54 +0000

    Fix Binary Sensor

--- custom_components/mastertherm/binary_sensor.py
+++ custom_components/mastertherm/binary_sensor.py
@@ -62,4 +62,4 @@ class MasterthermBinarySensor(MasterthermEntity, BinarySensorEntity):
         """Return the Value."""
         return self.coordinator.data["modules"][self._module_key]["entities"][
             self._entity_key
-        ]["on"]
+        ]["heatpump_on"]
