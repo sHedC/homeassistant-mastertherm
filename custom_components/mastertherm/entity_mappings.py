2fe2e0e95e4d2919ea0a718a306260192bd9261c
Richard Holmes <richard@shedc.uk>
2022-12-06 13:00:24 +0000

    Add Configurable Entities Fixes #12

--- custom_components/mastertherm/entity_mappings.py
+++ custom_components/mastertherm/entity_mappings.py
@@ -1,1 +1,67 @@
 """Contains all the Entity Mappings from the Mastertherm Connector"""
+from dataclasses import dataclass
+
+from homeassistant.components.sensor import (
+    SensorEntityDescription,
+    SensorDeviceClass,
+    SensorStateClass,
+)
+from homeassistant.components.binary_sensor import (
+    BinarySensorEntityDescription,
+    BinarySensorDeviceClass,
+)
+from homeassistant.components.switch import SwitchEntityDescription, DEVICE_CLASS_SWITCH
+
+# Entity ID
+# Required
+#   Entity Type
+#   Device Type
+#   Name
+#   State Mapping
+# Optional
+#   Key
+#   icon
+#   State Class
+#   native unit of measure
+
+
+@dataclass
+class MasterthermSwitchEntityDescription(SwitchEntityDescription):
+    """Description for the Mastertherm switch entities."""
+
+
+@dataclass
+class MasterthermSensorEntityDescription(SensorEntityDescription):
+    """Description for the Mastertherm sensor entities."""
+
+
+@dataclass
+class MasterthermBinarySensorEntityDescription(BinarySensorEntityDescription):
+    """Description for the Mastertherm binary sensor entities."""
+
+
+SWITCH_TYPES: dict[str, MasterthermSwitchEntityDescription] = {
+    "hp_power_state": MasterthermSwitchEntityDescription(
+        key="heatpump_power",
+        name="Heatpump Power",
+        device_class=DEVICE_CLASS_SWITCH,
+        icon="mdi:power",
+    )
+}
+
+SENSOR_TYPES: dict[str, MasterthermSensorEntityDescription] = {
+    "outside_temp": MasterthermSensorEntityDescription(
+        key="outside_temp",
+        name="Outside Temperature",
+        device_class=SensorDeviceClass.TEMPERATURE,
+        state_class=SensorStateClass.MEASUREMENT,
+    )
+}
+
+BINARY_SENSOR_TYPES: dict[str, MasterthermBinarySensorEntityDescription] = {
+    "hp_power_state": MasterthermBinarySensorEntityDescription(
+        key="heatpump_power_state",
+        name="Heatpump Power State",
+        device_class=BinarySensorDeviceClass.POWER,
+    )
+}
