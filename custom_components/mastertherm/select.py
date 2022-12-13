44633515dd16b268a0c633d491118d1d6a5f0a1e
Richard Holmes <richard@shedc.uk>
2022-12-13 20:39:41 +0000

    Another Select Try

--- custom_components/mastertherm/select.py
+++ custom_components/mastertherm/select.py
@@ -2,15 +2,11 @@
 import logging
 
 from homeassistant.core import HomeAssistant
-from homeassistant.components.select import (
-    SelectEntity,
-    ATTR_OPTION,
-    DOMAIN as SELECT_DOMAIN,
-    SERVICE_SELECT_OPTION,
-)
+from homeassistant.components.select import SelectEntity
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import CONF_ENTITIES, Platform
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.const import CONF_ENTITIES, Platform, ATTR_ENTITY_ID
+from homeassistant.exceptions import HomeAssistantError
 
 from .const import DOMAIN
 from .coordinator import MasterthermDataUpdateCoordinator
@@ -79,14 +75,4 @@ class MasterthermSelect(MasterthermEntity, SelectEntity):
 
     async def async_select_option(self, option: str) -> None:
         """Change the selected option."""
-        state = self.coordinator.data["modules"][self._module_key]["entities"][
-            self._entity_key
-        ]
-        current_option = self._reverse_map.get(state).lower()
-
-        if option != current_option:
-            self.hass.services.async_call(
-                SELECT_DOMAIN,
-                SERVICE_SELECT_OPTION,
-                {ATTR_OPTION: current_option, ATTR_ENTITY_ID: self.entity_id},
-            )
+        raise HomeAssistantError(f"Error while setting {self.name}")
