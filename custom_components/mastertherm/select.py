2012a62aca026a107349886da44ed4c35f53a056
Richard Holmes <richard@shedc.uk>
2023-01-29 19:38:40 +0000

    Attempt to block until previous command is done.

--- custom_components/mastertherm/select.py
+++ custom_components/mastertherm/select.py
@@ -130,11 +130,13 @@ class MasterthermSeasonSelect(MasterthermEntity, SelectEntity):
             await self.coordinator.update_state(
                 self._module_key, "season.manual_set", True
             )
+            await self.hass.async_block_till_done()
             await self.coordinator.update_state(self._module_key, "season.winter", True)
         else:
             await self.coordinator.update_state(
                 self._module_key, "season.manual_set", True
             )
+            await self.hass.async_block_till_done()
             await self.coordinator.update_state(
                 self._module_key, "season.winter", False
             )
