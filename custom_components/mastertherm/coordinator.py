cf72e65d5d641c0b531356e40ee55b18ed29bff0
Richard Holmes <richard@shedc.uk>
2023-01-30 19:17:47 +0000

    Lock to stop multiple requests to avoid failure.

--- custom_components/mastertherm/coordinator.py
+++ custom_components/mastertherm/coordinator.py
@@ -32,6 +32,8 @@ _LOGGER = logging.getLogger(__name__)
 class MasterthermDataUpdateCoordinator(DataUpdateCoordinator):
     """MasterTherm Device and Data Updater from single HTTPS Session."""
 
+    api_lock = asyncio.Lock()
+
     def __init__(
         self,
         hass: HomeAssistant,
@@ -122,87 +124,90 @@ class MasterthermDataUpdateCoordinator(DataUpdateCoordinator):
 
     async def _async_update_data(self) -> dict:
         """Refresh the data from the API endpoint and process."""
-        # Try to refresh, check for refresh issues
-        try:
-            if self.data is None or self.temporary_exception:
-                connected = await self.mt_controller.connect()
-                self.temporary_exception = False
+        async with self.api_lock:
+            # Try to refresh, check for refresh issues
+            try:
+                if self.data is None or self.temporary_exception:
+                    connected = await self.mt_controller.connect()
+                    self.temporary_exception = False
 
-            connected = await self.mt_controller.refresh()
+                connected = await self.mt_controller.refresh()
 
-            if not connected:
-                _LOGGER.error("Update Failed for unknown reason")
-                raise UpdateFailed("unknown_reason")
+                if not connected:
+                    _LOGGER.error("Update Failed for unknown reason")
+                    raise UpdateFailed("unknown_reason")
 
-        except MasterthermAuthenticationError as ex:
-            _LOGGER.error("Invalid credentials: %s", ex)
-            raise ConfigEntryAuthFailed("authentication_error") from ex
-        except MasterthermUnsupportedRole as ex:
-            _LOGGER.error("Unsupported role: %s", ex)
-            raise UpdateFailed("unsupported_role") from ex
-        except MasterthermConnectionError as ex:
-            _LOGGER.warning("Unable to communicate with MasterTherm API: %s", ex)
-            self.temporary_exception = True
-        except MasterthermTokenInvalid as ex:
-            _LOGGER.warning("Invalid Token: %s", ex)
-            self.temporary_exception = True
-        except MasterthermResponseFormatError as ex:
-            _LOGGER.warning("Response Format Error: %s", ex)
-            self.temporary_exception = True
+            except MasterthermAuthenticationError as ex:
+                _LOGGER.error("Invalid credentials: %s", ex)
+                raise ConfigEntryAuthFailed("authentication_error") from ex
+            except MasterthermUnsupportedRole as ex:
+                _LOGGER.error("Unsupported role: %s", ex)
+                raise UpdateFailed("unsupported_role") from ex
+            except MasterthermConnectionError as ex:
+                _LOGGER.warning("Unable to communicate with MasterTherm API: %s", ex)
+                self.temporary_exception = True
+            except MasterthermTokenInvalid as ex:
+                _LOGGER.warning("Invalid Token: %s", ex)
+                self.temporary_exception = True
+            except MasterthermResponseFormatError as ex:
+                _LOGGER.warning("Response Format Error: %s", ex)
+                self.temporary_exception = True
 
-        # If first run then populate the Modules.
-        result_data = self.data
-        if result_data is None:
-            result_data = {"modules": {}}
-            devices = self.mt_controller.get_devices()
-            for device_id, device in devices.items():
-                result_data["modules"][device_id] = {"info": device}
+            # If first run then populate the Modules.
+            result_data = self.data
+            if result_data is None:
+                result_data = {"modules": {}}
+                devices = self.mt_controller.get_devices()
+                for device_id, device in devices.items():
+                    result_data["modules"][device_id] = {"info": device}
 
-        # Retrieve the data and merge into the current data set based
-        # based on the sensor configuration.
-        for device_id, device in result_data["modules"].items():
-            device_data = self.mt_controller.get_device_data(
-                device["info"]["module_id"], device["info"]["unit_id"]
-            )
-
-            # Process Device Data and populate the data to pass to the Entities
-            if "entities" in result_data["modules"][device_id]:
-                result_data["modules"][device_id]["entities"].update(
-                    self.__build_entities("", device_data)
-                )
-            else:
-                result_data["modules"][device_id]["entities"] = self.__build_entities(
-                    "", device_data
+            # Retrieve the data and merge into the current data set based
+            # based on the sensor configuration.
+            for device_id, device in result_data["modules"].items():
+                device_data = self.mt_controller.get_device_data(
+                    device["info"]["module_id"], device["info"]["unit_id"]
                 )
 
-        return result_data
+                # Process Device Data and populate the data to pass to the Entities
+                if "entities" in result_data["modules"][device_id]:
+                    result_data["modules"][device_id]["entities"].update(
+                        self.__build_entities("", device_data)
+                    )
+                else:
+                    result_data["modules"][device_id][
+                        "entities"
+                    ] = self.__build_entities("", device_data)
+
+            asyncio.sleep(0.5)
+            return result_data
 
     async def update_state(self, module_key: str, entity_key: str, state: any):
         """Attempt to Update the State, data is in dot notation to get parent, child."""
-        # Get the Module and Unit ID.
-        module_id = self.data["modules"][module_key]["info"]["module_id"]
-        unit_id = self.data["modules"][module_key]["info"]["unit_id"]
+        async with self.api_lock:
+            # Get the Module and Unit ID.
+            module_id = self.data["modules"][module_key]["info"]["module_id"]
+            unit_id = self.data["modules"][module_key]["info"]["unit_id"]
 
-        return_value = False
-        try:
-            return_value = await self.mt_controller.set_device_data_item(
-                module_id, unit_id, entity_key, state
-            )
-        except MasterthermConnectionError as mex:
-            _LOGGER.warning("Unable to communicate with MasterTherm API: %s", mex)
-            self.temporary_exception = True
-        except MasterthermAuthenticationError as mex:
-            _LOGGER.error("Invalid credentials: %s", mex)
-            raise ConfigEntryAuthFailed("authentication_error") from mex
-        except MasterthermEntryNotFound as mex:
-            _LOGGER.warning("Entity not found or Read Only %s", mex)
+            return_value = False
+            try:
+                return_value = await self.mt_controller.set_device_data_item(
+                    module_id, unit_id, entity_key, state
+                )
+            except MasterthermConnectionError as mex:
+                _LOGGER.warning("Unable to communicate with MasterTherm API: %s", mex)
+                self.temporary_exception = True
+            except MasterthermAuthenticationError as mex:
+                _LOGGER.error("Invalid credentials: %s", mex)
+                raise ConfigEntryAuthFailed("authentication_error") from mex
+            except MasterthermEntryNotFound as mex:
+                _LOGGER.warning("Entity not found or Read Only %s", mex)
 
-        # Update data internally, on failure will reset back.
-        if return_value:
-            self.data["modules"][module_key]["entities"][entity_key] = state
+            # Update data internally, on failure will reset back.
+            if return_value:
+                self.data["modules"][module_key]["entities"][entity_key] = state
 
-        # Sleep for 1 second before returning so we don't throttle the API
-        asyncio.sleep(0.5)
+            # Sleep for 1 second before returning so we don't throttle the API
+            asyncio.sleep(0.5)
 
     def get_state(self, module_key: str, entity_key: str) -> any:
         """Get the State from the core data."""
