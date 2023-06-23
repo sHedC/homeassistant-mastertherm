ff2778c91935e35fffbfeefed6e66207363361dc
Richard Holmes <richard@shedc.uk>
2023-06-23 20:14:13 +0000

    bump version 1.1.5-b2 -> 1.1.5

--- custom_components/mastertherm/const.py
+++ custom_components/mastertherm/const.py
@@ -15,7 +15,7 @@ from homeassistant.components.switch import SwitchEntityDescription
 
 NAME = "Mastertherm"
 DOMAIN = "mastertherm"
-VERSION = "1.1.5-b2"
+VERSION = "1.1.5"
 
 API_VERSIONS = {
     "v1": "mastertherm.vip-it.cz (< 2022)",
